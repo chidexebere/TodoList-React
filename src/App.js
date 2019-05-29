9838ee0a3e6f41a1f7560301d32f4a8f95910582
Chidiebere <chidex5@gmail.com>
2019-05-29 23:25:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoItems from "./components/TodoItems";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addItem(e) {
+    if (this._inputElement.value !== "") {
+      let newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+
+      this.setState(prevState => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      });
+      this._inputElement.value = "";
+    }
+
+    e.preventDefault();
+  }
+
+  deleteItem(key) {
+    let filteredItems = this.state.items.filter(item => {
+      return item.key !== key;
+    });
+
+    this.setState({
+      items: filteredItems
+    });
+  }
+
+  render() {
+    return (
+      <div className="todoList">
+        <header className="header">
+          <h1>Todo</h1>
+
+          <form className="form" onSubmit={this.addItem}>
+            <input
+              className="input"
+              ref={a => (this._inputElement = a)}
+              placeholder="enter task"
+            />
+            <button className="button" type="submit">
+              add
+            </button>
+          </form>
+        </header>
+        <TodoItems entries={this.state.items} delete={this.deleteItem} />
+      </div>
+    );
+  }
 }
 
 export default App;
