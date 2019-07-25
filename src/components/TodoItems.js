c18b6c40ee78447033b9a20fd15f70c667870ed2
Chidiebere <chidex5@gmail.com>
2019-07-25 17:53:59 +0100

    feat: Add Build

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -4,20 +4,20 @@ import FlipMove from "react-flip-move";
 class TodoItems extends React.Component {
   constructor(props) {
     super(props);
-    this.createTasks = this.createTasks.bind(this);
+    //this.createTasks = this.createTasks.bind(this);
   }
-  createTasks(item) {
+  createTasks = item => {
     return (
       <li onClick={() => this.delete(item.key)} key={item.key}>
         <i class="fas fa-trash-alt" />
         {item.text}
       </li>
     );
-  }
+  };
 
-  delete(key) {
+  delete = key => {
     this.props.delete(key);
-  }
+  };
   render() {
     let todoEntries = this.props.entries;
     let listItems = todoEntries.map(this.createTasks);
