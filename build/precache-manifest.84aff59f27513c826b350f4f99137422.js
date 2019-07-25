87422ab0aa70a1ebe8954337ed329b17288fa42b
Chidiebere <chidex5@gmail.com>
2019-07-25 17:54:39 +0100

    feat: Add Build

---
+++ build/precache-manifest.84aff59f27513c826b350f4f99137422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9525c5a3c5a06c696a37a4067789d8e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08d23335fc99ee53770",
+    "url": "/static/css/main.1afd22a9.chunk.css"
+  },
+  {
+    "revision": "257a2d46b0a6a02cd04e",
+    "url": "/static/js/2.37c0fd2d.chunk.js"
+  },
+  {
+    "revision": "e08d23335fc99ee53770",
+    "url": "/static/js/main.9857f76e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
